22bfd17f1869bd0aa6a479e4ccc0237e0dd4b66e
stefchrys <chrysinus@gmail.com>
2014-10-29 16:07:34 +0100

    Commentaires propres pour phpdoc

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -1,6 +1,12 @@
 ﻿<?php
-//Si  'action'=null c'est qu'on essaye de se connecter
-//on lui attribut donc la valeur par defaut 'demandeConnexion'
+/**
+ * Verification et redirection d'un utilisateur
+ * 
+ * Redirige l'utilisateur qui essaye de se connecter vers le sommaire si 
+ * la connexion est validée sinon vers la page connexion .
+ */
+
+
 if(!isset($_REQUEST['action'])){
 	$_REQUEST['action'] = 'demandeConnexion';
 }
@@ -17,15 +23,15 @@ switch($action){
 		$login = $_REQUEST['login'];
 		$mdp = $_REQUEST['mdp'];
 		$visiteur = $pdo->getInfosVisiteur($login,$mdp);                
-                //si $visiteur n'est pas un tableau on renvoie page connexion
+                //si $visiteur n'est pas un tableau on renvoie vers connexion
 		if(!is_array( $visiteur)){
 			ajouterErreur("Login ou mot de passe incorrect");
 			include("vues/v_erreurs.php");
 			include("vues/v_connexion.php");
 		}
-                //si $visiteur est un tableau associatif alors on se connecte
-                //au sommaire en recuperant les valeur du tableau
-                //et en les assignant aux variables de SESSION
+                /*si $visiteur est un tableau associatif alors on se connecte
+                au sommaire en récuperant les valeurs du tableau
+                et en les assignant aux variables de SESSION*/
 		else{
 			$id = $visiteur['id'];
 			$nom =  $visiteur['nom'];
