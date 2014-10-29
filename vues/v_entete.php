ba96910f336e74251c8a585bbefe4ac0ca3ac56b
stefchrys <chrysinus@gmail.com>
2014-10-29 17:08:31 +0100

    Commentaires propres pour phpdoc 2

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>  
 <html lang="fr">
-  <head>
-    <title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
-    <meta charset="UTF-8" />
-    <link href="./styles/styles.css" rel="stylesheet"/>
-    <link rel="shortcut icon"  href="./images/favicon.ico" />
-  </head>
-  <body>
-    <div id="page">
-      <div id="entete">
-        <img src="./images/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss
-             Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
-        <h1>Suivi du remboursement des frais</h1>
-      </div>
+    <head>
+        <title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
+        <meta charset="UTF-8" />
+        <link href="./styles/styles.css" rel="stylesheet"/>
+        <link rel="shortcut icon"  href="./images/favicon.ico" />
+    </head>
+    <body>
+        <div id="page">
+            <div id="entete">
+                <img src="./images/logo.jpg" id="logoGSB" alt="Laboratoire Galaxy-Swiss
+                     Bourdin" title="Laboratoire Galaxy-Swiss Bourdin" />
+                <h1>Suivi du remboursement des frais</h1>
+            </div>
