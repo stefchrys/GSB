ff295b248f551c36e3622688275527b33ec84cff
stefchrys <chrysinus@gmail.com>
2014-10-29 16:10:12 +0100

    Commentaires propres pour phpdoc

--- controleurs/c_etatFrais.php
+++ controleurs/c_etatFrais.php
@@ -1,13 +1,16 @@
 ﻿<?php
+/**
+ * Fichier qui gère l'affichage des frais
+ */
 include("vues/v_sommaire.php");
 $action = $_REQUEST['action'];
 $idVisiteur = $_SESSION['idVisiteur'];
 switch($action){
 	case 'selectionnerMois':{
 		$lesMois=$pdo->getLesMoisDisponibles($idVisiteur);
-		// Afin de sélectionner par défaut le dernier mois dans la zone de liste
-		// on demande toutes les clés, et on prend la première,
-		// les mois étant triés décroissants
+		/* Afin de sélectionner par défaut le dernier mois dans la zone de liste
+		 on demande toutes les clés, et on prend la première,
+		 les mois étant triés de manière décroissante */
 		$lesCles = array_keys( $lesMois );
 		$moisASelectionner = $lesCles[0];
 		include("vues/v_listeMois.php");
