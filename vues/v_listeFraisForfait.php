1c8fc7cdc6d1c85121730388796059af7b95bbc0
stefchrys <chrysinus@gmail.com>
2014-10-29 16:49:55 +0100

    Commentaires propres pour phpdoc 2

--- vues/v_listeFraisForfait.php
+++ vues/v_listeFraisForfait.php
@@ -1,27 +1,41 @@
+<?php
+/**
+ * Affichage Liste frais
+ * @author chrysinus@gmail.com
+ */
+?>
 <div id="contenu">
-      <h2>Renseigner ma fiche de frais du mois <?php echo $numMois."-".$numAnnee ?></h2>
+      <h2>Renseigner ma fiche de frais du mois 
+          <?php echo $numMois."-".$numAnnee ?></h2>
          
-      <form method="POST"  action="index.php?uc=gererFrais&action=validerMajFraisForfait">
+      <form method="POST"  
+            action="index.php?uc=gererFrais&action=validerMajFraisForfait">
       <div class="corpsForm">
           
           <fieldset>
             <legend>Eléments forfaitisés
             </legend>
-			<?php
-				foreach ($lesFraisForfait as $unFrais)
-				{
-					$idFrais = $unFrais['idfrais'];
-					$libelle = $unFrais['libelle'];
-					$quantite = $unFrais['quantite'];
-			?>
-					<p>
-						<label for="idFrais"><?php echo $libelle ?></label>
-						<input type="text" id="idFrais" name="lesFrais[<?php echo $idFrais?>]" size="10" maxlength="5" value="<?php echo $quantite?>" >
-					</p>
+                <?php
+                    foreach ($lesFraisForfait as $unFrais){
+                        $idFrais = $unFrais['idfrais'];
+                        $libelle = $unFrais['libelle'];
+                        $quantite = $unFrais['quantite'];
+                ?>
+                        <p>
+                            <label for="idFrais">
+                            <?php echo $libelle ?></label>
+                            <input 
+                            type="text" 
+                            id="idFrais" 
+                            name="lesFrais[<?php echo $idFrais?>]" 
+                            size="10" 
+                            maxlength="5" 
+                            value="<?php echo $quantite?>" >
+                        </p>
 			
-			<?php
-				}
-			?>
+                 <?php
+                    }
+                 ?>
 			
 			
 			
