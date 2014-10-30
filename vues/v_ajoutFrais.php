ecdc78d2677febf3d147a653aba0df76f4ce40a3
stefchrys <chrysinus@gmail.com>
2014-10-30 09:55:19 +0100

    ajout prefix sur elements formulaires html

--- vues/v_ajoutFrais.php
+++ vues/v_ajoutFrais.php
@@ -5,33 +5,33 @@
         <tr>
             <td>Date du frais (jj/mois/aaaa)</td>
             <td>
-                <input  type='text' name=dateFrais  size='30' maxlength='45'>
+                <input  type='text' name='txt_dateFrais'  size='30' maxlength='45'>
             </td>
         </tr>
         <tr>
             <td>Description du frais</td>
             <td>
-                <input  type='text' name=description  size='50' maxlength='100'>
+                <input  type='text' name='txt_description' size='50' maxlength='100'>
             </td>
         </tr>
         <tr>
             <td>Montant engage</td>
             <td>
-                <input  type='text' name=montant  size='30' maxlength='45'>
+                <input  type='text' name='txt_montant'  size='30' maxlength='45'>
             </td>
         </tr>
         <tr>
             <td>Justificatif</td>
-            <td><input type='radio' name='justificatif' value='oui'> oui
+            <td><input type='radio' name='opt_justificatif' value='oui'> oui
             </td>
             <td>
-                <input type='radio' name='justificatif' value='non'> non
+                <input type='radio' name='opt_justificatif' value='non'> non
             </td>
 
         </tr>
 
     </table>
-    <input type='submit' value='Valider' name='valider'>
-    <input type='reset' value='Annuler' name='annuler'>
+    <input type='submit' value='Valider' name='cmd_valider'>
+    <input type='reset' value='Annuler' name='br_annuler'>
 
 </form>
