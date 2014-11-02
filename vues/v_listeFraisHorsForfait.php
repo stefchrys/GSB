e88adffe4e1fea20adcfae37d2a156790301623b
stefchrys <chrysinus@gmail.com>
2014-11-02 13:32:45 +0100

    test bootstrap

--- vues/v_listeFraisHorsForfait.php
+++ vues/v_listeFraisHorsForfait.php
@@ -1,67 +1,97 @@
-﻿<!-- v_listeFraisHorsForfait -->
-<table class="listeLegere">
-  	   <caption>Descriptif des éléments hors forfait
-       </caption>
-             <tr>
-                <th class="date">Date</th>
-				<th class="libelle">Libellé</th>  
-                <th class="montant">Montant</th>  
-                <th>&nbsp;</th>              
-             </tr>
-          
-    <?php    
-	    foreach( $lesFraisHorsForfait as $unFraisHorsForfait) 
-		{
-			$libelle = $unFraisHorsForfait['libelle'];
-			$date = $unFraisHorsForfait['date'];
-			$montant=$unFraisHorsForfait['montant'];
-			$id = $unFraisHorsForfait['id'];
-	?>		
-            <tr>
-                <td> <?php echo $date ?></td>
-                <td><?php echo $libelle ?></td>
-                <td><?php echo $montant ?></td>
-                <td><a href="index.php?uc=gererFrais&action=supprimerFrais&idFrais=<?php echo $id ?>" 
-				onclick="return confirm('Voulez-vous vraiment supprimer ce frais?');">Supprimer ce frais</a></td>
-             </tr>
-	<?php		 
-          
-          }
-	?>	  
-                                          
-    </table>
-      <form action="index.php?uc=gererFrais&action=validerCreationFrais" method="post">
-      <div class="corpsForm">
-         
-          <fieldset>
-            <legend>Nouvel élément hors forfait
-            </legend>
-            <p>
-              <label >Date (jj/mm/aaaa): </label>
-              <input type="text"  name="dateFrais" size="10" maxlength="10"
-                     value="" required/>
-            </p>
-            <p>
-              <label >Libellé</label>
-              <input type="text"  name="libelle" size="70" maxlength="256" 
-                     value="" required/>
-            </p>
-            <p>
-              <label >Montant : </label>
-              <input type="text"  name="montant" size="10" maxlength="10" 
-                     value="" required/>
-            </p>
-          </fieldset>
-      </div>
-      <div >
-      <p>
-        <input id="ajouter" type="submit" value="Ajouter"  />
-        <input id="effacer" type="reset" value="Effacer"  />
-      </p> 
-      </div>
-        
-      </form>
-  </div>
-  <!-- fin v_listeFraisHorsForfait -->
-  
+<!--debut v_listehorsFrais -->
+<div class="row clearfix"  >
+    <div class="col-md-4 column">		
+    </div>
+    <div class="col-md-8 column contenu">
 
+
+        <h3 class='text-center'>
+            Descriptif des éléments hors forfaits .
+        </h3>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+
+                    <th>
+                        <span class="label label-info">Date</span>
+
+                    </th>
+                    <th>
+
+                        <span class="label label-info">Libellé</span>
+
+                    </th>
+                    <th>
+
+                        <span class="label label-info">Montant</span>
+                    </th>
+                    <th>
+                        &nbsp
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($lesFraisHorsForfait as $unFraisHorsForfait) {
+                    $libelle = $unFraisHorsForfait['libelle'];
+                    $date = $unFraisHorsForfait['date'];
+                    $montant = $unFraisHorsForfait['montant'];
+                    $id = $unFraisHorsForfait['id'];
+                    ?>	
+                    <tr class="active">
+                        <td>
+                            <?php echo $date ?>
+                        </td>
+                        <td>
+                            <?php echo $libelle ?>
+                        </td>
+                        <td>
+                            <?php echo $montant ?>
+                        </td>
+
+                        <td>
+                            <a href="index.php?uc=gererFrais&action=supprimerFrais&idFrais=<?php echo $id ?>" 
+                               onclick="return confirm('Voulez-vous vraiment supprimer ce frais?');">Supprimer ce frais</a>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
+
+            </tbody>
+        </table>
+
+        <form class="form-horizontal contenu" role="form" action="index.php?uc=gererFrais&action=validerCreationFrais" method="post">
+            <fieldset>
+                <legend>Nouvel élément hors-forfait.
+                </legend>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Date(jj/mm/aaaa):</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" name="dateFrais" type="text" 
+                               value ="" required/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Libelle</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" name="dateFrais" type="text" 
+                               value ="" required/>						
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail3" class="col-sm-2 control-label">Montant</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" name="dateFrais" type="text" 
+                               value ="" required/>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Ajouter</button>
+                        </div>
+                    </div>
+            </fieldset>
+        </form>
+    </div>		
+</div>
+<!--fin v_listehorsFrais -->
