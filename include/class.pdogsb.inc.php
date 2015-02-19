9db0e120e01a867f8fae7aca3fc1107f80f601b0
stefchrys <chrysinus@gmail.com>
2015-02-19 18:04:16 +0100

    bircoles à changer

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -363,8 +363,10 @@ class PdoGsb {
 
     /**
      * Retourne les mois pour lesquel un visiteur a une fiche de frais
-
-     * @param string $idVisiteur 
+     
+     * @param string $idVisiteur
+     * @param $periode string "all" selectionne tout les mois disponibles
+     * "year" selectionne les 12 derniers mois 
      * @return array Tableau associatif de clé un mois -aaaamm- et de valeurs l'année 
      * et le mois correspondant 
      */
